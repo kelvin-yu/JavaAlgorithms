d267eb2c94fc378fe2e527ab8ebca8ecfdc995a6
kelvin-yu <kelvin.yu98@gmail.com>
2015-03-13 19:45:43 -0400

    initial version'

---
+++ src/Math.java
@@ -0,0 +1,32 @@
+
+public class Math {
+	/*------------------------------------------------------------------*/
+	//Finds lcm and gcd
+	private static long gcd(long a, long b)
+	{
+	    while (b > 0)
+	    {
+	        long temp = b;
+	        b = a % b;
+	        a = temp;
+	    }
+	    return a;
+	}
+	private static long gcd(long[] input)
+	{
+	    long result = input[0];
+	    for(int i = 1; i < input.length; i++) result = gcd(result, input[i]);
+	    return result;
+	}
+	private static long lcm(long a, long b)
+	{
+	    return a * (b / gcd(a, b));
+	}
+
+	private static long lcm(long[] input)
+	{
+	    long result = input[0];
+	    for(int i = 1; i < input.length; i++) result = lcm(result, input[i]);
+	    return result;
+	}
+}
---
+++ src/Utils.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Utils {
+	public static class Reader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public Reader(String s) {
+			try {
+				br = new BufferedReader(new FileReader(s));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public Reader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String nextToken() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(nextToken());
+		}
+
+		long nextLong() {
+			return Long.parseLong(nextToken());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(nextToken());
+		}
+	}
+}
