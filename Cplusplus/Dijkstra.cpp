1f17f8e40eadc9650c2418761c5bcd9eb5e30d9e
kelvin-yu <kelvin.yu98@gmail.com>
2015-05-21 19:30:27 -0400

    Create Dijkstra.cpp

---
+++ Cplusplus/Dijkstra.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MAXN 300001
+
+vector<pair<int,int>> G[MAXN];
+int start;
+int N, M;
+int dist[MAXN];
+
+int main()
+{
+    priority_queue<pair<int, int>> q;
+    dist[start] = 0;
+    q.push(make_pair(0, start));
+    while(!q.empty()){
+        int cost = -q.top().first;
+        int cur = q.top().second;
+        if(dist[cur] < cost)
+            continue;
+        for(int i = 0; i < G[cur].size(); i++){
+            int target = G[cur][i].first;
+            int weight = G[cur][i].second;
+            if(dist[target] > dist[cur] + weight){
+                dist[target] = dist[cur] + weight;
+                q.push(make_pair(dist[target], target));
+            }
+        }
+    }
+}
