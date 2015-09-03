2f1cad6f30215066701464f50c06029ebf283a65
kelvin-yu <kelvin.yu98@gmail.com>
2015-09-03 01:35:17 -0400

    Create Execution_Time.cpp

---
+++ Cplusplus/Execution_Time.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.>
+using namespace std;
+
+int main()
+{
+    double t = clock();
+    while((clock()-t)/CLOCKS_PER_SEC < 1.0){} //approx a little more than 1 sec
+    cout << "Finished" << endl;
+}
