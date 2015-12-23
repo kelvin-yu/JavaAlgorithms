e4c64c40f284ef7cfec84a9ed8e8cd2edf8774ce
kelvin-yu <kelvin.yu98@gmail.com>
2015-12-22 21:08:01 -0500

    Create LCA_Doubling_Method.cpp

---
+++ Cplusplus/Graph-Theory/LCA_Doubling_Method.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MAXN 1000000
+#define LOGMAXN 20
+int pa[MAXN][LOGMAXN];
+int depth[MAXN];
+vector<int> E[MAXN];
+int N;
+
+void dfs(int cur, int d, int prev){
+    depth[cur] = d;
+    pa[cur][0] = prev;
+    for(auto e : E[cur]){
+        if(e != prev)
+            dfs(e, d+1, cur);
+    }
+}
+
+void build(){
+    dfs(0, 0, -1);
+    for(int i = 1; i < LOGMAXN; i++){
+        for(int j = 0; j < N; j++){
+            if(pa[j][i-1] != -1)
+                pa[j][i] = pa[pa[j][i-1]][i-1];
+        }
+    }
+}
+
+int lca(int i, int j){
+    if(depth[i] < depth[j])
+        swap(i, j);
+    for(int k = LOGMAXN-1; k >= 0; k--){
+        if(pa[i][k] != -1 && depth[pa[i][k]] >= depth[j])
+            i = pa[i][k];
+    }
+    if(i == j)
+        return i;
+    for(int k = LOGMAXN-1; k >= 0; k--){
+        if(pa[i][k] != -1 && pa[j][k] != -1 && pa[i][k] != pa[j][k]){
+            i = pa[i][k];
+            j = pa[j][k];
+        }
+    }
+    return pa[i][0];
+}
+
+int main()
+{
+
+}
